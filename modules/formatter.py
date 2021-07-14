677f8f0be60fe068e33e0e826f3a391f6d0589fe
Chittle Skuny <chittleskuny@gmail.com>
2021-07-14 13:59:52 +0800

    Add line number for formatter

--- modules/formatter.py
+++ modules/formatter.py
@@ -1,4 +1,4 @@
-def format_bit_string(bit_string, indent=0, indent_1st=True, column=8):
+def format_bit_string(bit_string, line_num=False, indent=0, indent_1st=True, column=8):
     if len(bit_string) % 8 != 0: # 8 bits per byte
         print('Not a multiple of 8.')
         return None
@@ -10,17 +10,29 @@ def format_bit_string(bit_string, indent=0, indent_1st=True, column=8):
         if (n % column == 0) and line:
             number = n - column + 1
             if flag_1st and not indent_1st:
-                lines.append(('$%d:\t' % number) + ' '.join(line))
+                if line_num:
+                    lines.append(('$%d:\t' % number) + ' '.join(line))
+                else:
+                    lines.append(' '.join(line))
                 flag_1st = not flag_1st
             else:
-                lines.append(indent + ('$%d:\t' % number) + ' '.join(line))
+                if line_num:
+                    lines.append(indent + ('$%d:\t' % number) + ' '.join(line))
+                else:
+                    lines.append(indent + ' '.join(line))
             line = []
         byte = bit_string[(n*8):((n + 1)*8)]
         line.append(byte)
     if line:
         number = n - len(line) + 1
         if flag_1st and not indent_1st:
-            lines.append(('$%d:\t' % number) + ' '.join(line))
+            if line_num:
+                lines.append(('$%d:\t' % number) + ' '.join(line))
+            else:
+                lines.append(' '.join(line))
         else:
-            lines.append(indent + ('$%d:\t' % number) + ' '.join(line))
+            if line_num:
+                lines.append(indent + ('$%d:\t' % number) + ' '.join(line))
+            else:
+                lines.append(indent + ' '.join(line))
     return '\n'.join(lines)
